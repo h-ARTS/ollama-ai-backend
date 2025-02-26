831f5e8e0c9f6e164e52a9dc5e72aa248c0abc26
Hanan Mufti <hi5hanan@yahoo.com>
2025-02-26 12:46:18 +0100

    increase multipart size and finetune prompt

--- src/main/kotlin/com/hanankhan/ollamaai/ollamaai/controller/ImageDetectionController.kt
+++ src/main/kotlin/com/hanankhan/ollamaai/ollamaai/controller/ImageDetectionController.kt
@@ -1,16 +1,19 @@
 package com.hanankhan.ollamaai.ollamaai.controller
 
+import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.model.Media
 import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.ai.ollama.api.OllamaOptions
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.MimeType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.io.IOException
-import java.util.Base64
 
 @RestController
 @RequestMapping("/ai")
@@ -21,19 +24,25 @@ class ImageDetectionController(
     @PostMapping("/generate")
     fun generate(@RequestParam("image") image: MultipartFile): ResponseEntity<String> {
         return try {
-            val base64Image = Base64.getEncoder().encodeToString(image.bytes)
+            val media = Media(MediaType.IMAGE_PNG, image.resource)
 
-            val prompt = "Extract text from this image carefully!"
-            val fullPrompt = "$prompt\n\n![image](data:image/png;base64,$base64Image)"
+            val prompt = """I have provided you an image and you should NOT explain what the image entails. 
+                Instead you should parse the content and give it as an output here. Please no additional information. 
+                You must NOT omit anything in the image even if they appear to be visualized in someway.
+                It should be also formatted in json like so:
+                {
+                    content: 'your output'
+                }""".trimMargin()
+            val userMessage = UserMessage(prompt, listOf(media))
 
             val response = this.chatModel.call(
                 Prompt(
-                    fullPrompt,
-                    OllamaOptions.builder().model("deepseek-r1:latest").build()
+                    userMessage,
+                    OllamaOptions.builder().model("llama3.2-vision").build()
                 )
             )
 
-            return ResponseEntity.ok(response.result.output.text)
+            ResponseEntity.ok(response.result.output.text)
         } catch (ex: IOException) {
             ex.printStackTrace()
             ResponseEntity.status(500).body("Error processing the image: ${ex.message}")
