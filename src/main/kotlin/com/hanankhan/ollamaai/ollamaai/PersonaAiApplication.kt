b7b6651520ccaaf494a3f1cbe0554fb3634b935a
Hanan Mufti <hi5hanan@yahoo.com>
2025-04-24 23:10:52 +0200

    rename main class

--- src/main/kotlin/com/hanankhan/ollamaai/ollamaai/PersonaAiApplication.kt
+++ src/main/kotlin/com/hanankhan/ollamaai/ollamaai/PersonaAiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class OllamaAiApplication
+class PersonaAiApplication
 
 fun main(args: Array<String>) {
-    runApplication<OllamaAiApplication>(*args)
+    runApplication<PersonaAiApplication>(*args)
 }
